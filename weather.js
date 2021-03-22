2027e4f6af9c821fd18575402d32e4ebbb6c8603
dfloren <daniel_florendo1@yahoo.com>
2021-03-21 22:52:32 -0600

    added API call to api.weatherapi.com added credit to api

--- weather.js
+++ weather.js
@@ -1,13 +1,23 @@
 window.addEventListener('load', () => {
-    let long;
+    let lon;
     let lat;
+    let key = 'a10697011b3f441786b32529212203'; // pls don't blow up my poor, unsecured API key :(
 
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
             lat = position.coords.latitude;
-        })
-    } else {
-        document.getElementsByTagName('h1')[0].textContent = "Please enable geolocation on your browser";
+            lon = position.coords.longitude;
+
+            const api = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${lat},${lon}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    alert(JSON.stringify(data));
+                    document.getElementById("response-json").innerHTML= JSON.stringify(data);
+                })
+        });
     }
 });
